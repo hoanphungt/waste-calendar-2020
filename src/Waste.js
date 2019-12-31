6630f10538f20ccef23856ea8568af6ec0cdb55a
hoanphungt <hoanphung.cpo@gmail.com>
2019-12-31 10:25:34 +0100

    Initial setup for the waste calendar app

---
+++ src/Waste.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class WasteCalendar extends React.Component {
+  render() {
+    return (
+      <div className="WasteCalendar">
+        Waste Calendar
+      </div>
+    );
+  }
+}
